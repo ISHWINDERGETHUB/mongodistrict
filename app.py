8ae5203711bf96a21a4735e788e0220deba23073
ISHWINDERGETHUB <62585944+ISHWINDERGETHUB@users.noreply.github.com>
2020-10-24 22:47:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ def table_fkj(m):
     {"$and":[{'USER_ID.USER_NAME':{"$not":{"$regex":"Test",'$options':'i'}}},
     {'USER_ID.USER_NAME':{"$not":{"$regex":'1gen','$options':'i'}}}]}}
     ,
-    {"$project":{"_id":0,"USER_ID":"$USER_ID._id","practice_date":{ "$dateToString": {"format": "%Y-%m-%d", "date": "$MODIFIED_DATE"} }}}
+    {"$project":{"_id":0,"USER_ID":"$USER_ID._id","practice_date":{ "$dateToString": {"format": "%Y-%m-%d %H:%M:%S", "date": "$MODIFIED_DATE"} }}}
 
     ])
     df3= DataFrame(list(collection2)).fillna(0)
@@ -404,7 +404,7 @@ def table_fhj(m):
     {"$and":[{'USER_ID.USER_NAME':{"$not":{"$regex":"Test",'$options':'i'}}},
     {'USER_ID.USER_NAME':{"$not":{"$regex":'1gen','$options':'i'}}}]}}
     ,
-    {"$project":{"_id":0,"USER_ID":"$USER_ID._id","practice_date":{ "$dateToString": {"format": "%Y-%m-%d", "date": "$MODIFIED_DATE"} }}}
+    {"$project":{"_id":0,"USER_ID":"$USER_ID._id","practice_date":{ "$dateToString": {"format":  "%Y-%m-%d %H:%M:%S", "date": "$MODIFIED_DATE"} }}}
 
     ])
     df3= DataFrame(list(collection2)).fillna(0)
@@ -412,6 +412,61 @@ def table_fhj(m):
     final1=final[final["practice_date"]!= 0 ]
     df6=final1.groupby(['USER_ID'])['practice_date'].max().reset_index()
     df6=df6[df6["practice_date"]<"2020-08-01"]
+    user_detail=pd.merge(df6, final1, on='USER_ID',how='left').fillna(0)
+    export=user_detail[["USER_NAME","email_id","school_name","practice_date_y"]].values.tolist()
+    temp={"data":export}
+    return(json.dumps(temp))
+
+@app.route('/distable/<m>/csy')
+def table_fxs(m):
+    username = urllib.parse.quote_plus('admin')
+    password = urllib.parse.quote_plus('A_dM!n|#!_2o20')
+    client = MongoClient("mongodb://%s:%s@44.234.88.150:27017/" % (username, password))
+    db=client.compass
+    collection = db.user_master.aggregate([
+    {"$match":{"schoolId":{"$exists":1}}},
+    {"$match":
+        {"$and":[
+        {"DISTRICT_ID._id":ObjectId(""+m+"")},
+             {'ROLE_ID._id':{'$ne':ObjectId("5f155b8a3b6800007900da2b")}},
+        {'IS_DISABLED':{"$ne":'Y'}},
+    {'IS_BLOCKED':{"$ne":'Y'}}, 
+    {'INCOMPLETE_SIGNUP':{"$ne":'Y'}},
+    {'schoolId.NAME':{"$not":{"$regex":'Blocked', '$options':'i'}}}]}},
+    {"$match":
+    {"$and":[{'USER_NAME':{"$not":{"$regex":"Test",'$options':'i'}}},
+    {'USER_NAME':{"$not":{"$regex":'1gen','$options':'i'}}}]}}
+    ,
+    {"$project":{"USER_ID":"$_id","ID":"$schoolId._id","school_name":"$schoolId.NAME","USER_NAME":"$USER_NAME",
+                "email_id":"$EMAIL_ID","district_name":"$DISTRICT_ID.DISTRICT_NAME"}}
+
+    ])
+    df1= DataFrame(list(collection)).fillna(0)
+    user_list=df1["USER_ID"].tolist()
+    collection2 = db.audio_track_master.aggregate([
+    {"$match":{"USER_ID._id":{
+                        "$in":user_list
+
+                    }    ,"USER_ID.schoolId":{"$exists":1}}},
+    {"$match":
+        {"$and":[
+        {'USER_ID.IS_DISABLED':{"$ne":'Y'}},
+    {'USER_ID.IS_BLOCKED':{"$ne":'Y'}}, 
+    {'USER_ID.INCOMPLETE_SIGNUP':{"$ne":'Y'}},
+    {'MODIFIED_DATE':{"$gt":datetime.datetime(2019,7,31)}},
+    {'USER_ID.schoolId.NAME':{"$not":{"$regex":'Blocked', '$options':'i'}}}]}},
+    {"$match":
+    {"$and":[{'USER_ID.USER_NAME':{"$not":{"$regex":"Test",'$options':'i'}}},
+    {'USER_ID.USER_NAME':{"$not":{"$regex":'1gen','$options':'i'}}}]}}
+    ,
+    {"$project":{"_id":0,"USER_ID":"$USER_ID._id","practice_date":{ "$dateToString": {"format":  "%Y-%m-%d %H:%M:%S", "date": "$MODIFIED_DATE"} }}}
+
+    ])
+    df3= DataFrame(list(collection2)).fillna(0)
+    final=pd.merge(df1, df3, on='USER_ID',how='left').fillna(0)
+    final1=final[final["practice_date"]!= 0 ]
+    df6=final1.groupby(['USER_ID'])['practice_date'].max().reset_index()
+    df6=df6[df6["practice_date"]>"2020-07-31"]
     user_detail=pd.merge(df6, final1, on='USER_ID',how='left').fillna(0)
     export=user_detail[["USER_NAME","email_id","school_name","practice_date_y"]].values.tolist()
     temp={"data":export}
