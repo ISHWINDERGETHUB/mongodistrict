fd6dc3071401308d828b5f999a3a1a7c6714002c
Bhupinder <62585944+ISHWINDERGETHUB@users.noreply.github.com>
2022-02-21 20:45:52 +0530

    commit

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    var max_fields = 10;
+    var wrapper = $(".container1");
+    var add_button = $(".add_form_field");
+  
+    var x = 3;
+    $(add_button).click(function(e) {
+        e.preventDefault();
+        if (x < max_fields) {
+            x++;
+            $(wrapper).append('<div class="input-field col s4"><label for="s'+x+'"><b>Symptom-'+x+'</b></label><br><input id="s'+x+'" name="Symptom'+x+'" placeholder="Symptoms'+x+'" type="text" class="validate"><a href="#" class="delete">Delete</a></div>'); //add input box
+        } else {
+            alert('You Reached the limits')
+        }
+    });
+  
+    $(wrapper).on("click", ".delete", function(e) {
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+  });
